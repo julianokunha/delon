73a0606eb2f9af61a80e53dc48fb011aad1e81ae
Juliano Carvalho <julianokunha@gmail.com>
2021-02-26 15:03:59 -0300

    Create translate to pt_BR

--- packages/theme/src/locale/public_api.ts
+++ packages/theme/src/locale/public_api.ts
@@ -8,6 +8,7 @@ export { default as zh_CN } from './languages/zh-CN';
 export { default as zh_TW } from './languages/zh-TW';
 export { default as tr_TR } from './languages/tr-TR';
 export { default as pl_PL } from './languages/pl-PL';
+export { default as pt_BR } from './languages/pt-BR';
 export { default as el_GR } from './languages/el-GR';
 export { default as ko_KR } from './languages/ko-KR';
 export { default as hr_HR } from './languages/hr-HR';
